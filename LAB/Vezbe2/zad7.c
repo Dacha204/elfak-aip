2ea1732d81af8e9ead1bc67285d38ffbb0ed12f4
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2015-03-24 00:07:13 +0100

    Create zad7.c

---
+++ LAB/Vezbe2/zad7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	
+	float y;
+	int x,n;
+	
+	scanf("%d",&n);
+	
+	for (x=-n; x<=n; x++){
+		if (x < 0)
+			y = x*x;
+		else if (x >= 1)
+			y = sqrtf(x);
+		else
+			y = x;
+		
+		printf("%f\n",y);
+	
+	}
+	return 0;
+}
