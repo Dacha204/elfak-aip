51f5ae7b05bd545b89956bcb767bd3a3cce7eee4
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2015-03-24 19:39:52 +0100

    Update zad4.c

--- LAB/Vezbe2/zad4.c
+++ LAB/Vezbe2/zad4.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 
-int main(void){
+main(){
 	
 	int s,k,i,t;
 	
 	scanf("%d", &k);
 	
+	//ispitujemo redom brojeve od 100 do 999
 	for (i = 100; i<1000; i++)
+		
+		//deljiv sa k? ako nije testiramo naredni
 		if (i%k == 0){
+			
+			//kopiramo vrednost trenutnog broja i u priveremenu promenljivu
+			//jer naredni postupak unistava vrednost promenljive cije cifre sumiramo
 			t = i;
 			
-			//sumira cifre
-			for (s=0; t; t/=10)
-				s += t%10;
+			//sumira cifre (vidi prethodne zadatke)
+			s = 0;
+			while (t > 0){
+				s = s + (t % 10);
+				t = t / 10;
+			}
 			
+			//suma cifara == 17?
 			if (s == 17)
 				printf("%d\n",i);
 		}
 	
 	
-	return 0;
+
 }
