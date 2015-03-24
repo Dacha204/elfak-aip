e40c56138a09cb58942337e3f7dac59f81f571c1
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2015-03-24 20:14:22 +0100

    Update zad5.c

--- LAB/Vezbe2/zad5.c
+++ LAB/Vezbe2/zad5.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
 
-
-int main(void){
+void main(){
+	int sum, n, temp;
 	
-	int sum = 0, n, temp;
 	scanf("%d",&n);
 	
-	for (temp = n; temp; temp/=10)
-		sum += temp%10;
-	
-	printf("sum = %d\n",sum);
-	
-	printf("bin = ");
-	for (temp = n; temp; temp/=2){
-		printf("%d",temp%2);	
+	//sumira cifre zadatog broja (vidi prethodne zadatke)
+	sum = 0;
+	temp = n; //kopira vrednost n u temp promenjlivu jer postupak ispod bi unistio vrednost u n
+		  // dalji rad se vrsi nad promenljivom temp
+	while (temp > 0){
+		sum = sum + (temp % 10);
+		temp = temp / 10;
 	}
+	printf("sum = %d\n", sum);
+	
+// -- binarno
+	//stampa redom cifre koje pronadje
+	
+	printf("bin = ");	
+	temp = n; 	
+	while (temp > 0){
+		printf("%d",temp % 2);  // --binarno - osnova je 2 | stampa poslednju cifru
+		temp = temp / 2;		// krati poslednju cifru
+	}
+
+// -- octalno
 	
 	printf("\noct = ");
-	for (temp = n; temp; temp/=8){
-		printf("%d",temp%8);
+	temp = n; 
+	while (temp > 0){
+		printf("%d",temp % 8);  // --octalno - osnova je 8 | stampa poslednju cifru
+		temp = temp / 8;	// krati poslednju cifru
 	}
 	
+// -- heksadekadno (osnova 16)
 	printf("\nhex = ");
-	for (temp = n; temp; temp/=16){
-		switch (temp%16){
+	temp = n;
+	while (temp > 0){
+		switch (temp%16){ 		//ostatak moze bude i >9 
 			case 10: printf("A"); break;
 			case 11: printf("B"); break;
 			case 12: printf("C"); break;
@@ -31,19 +46,8 @@ int main(void){
 			case 14: printf("E"); break;
 			case 15: printf("F"); break;
 			default: printf("%d",temp%16);
-		}
-	}	
+		}  
+		temp = temp / 16;		// krati poslednju cifru
+	}
 
-	
-	/* umesto FOR moze:
-	 * 
-	 * temp = n;
-	 * while (temp > 0){
-	 * 		printf("%d",temp%16);
-	 * 		temp = temp / 16;
-	 * }
-	 * 
-	 */	
-	
-	return 0;
 }
