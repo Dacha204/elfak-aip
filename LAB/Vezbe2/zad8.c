8f4f869058801f8320be8370df8c8d8fccee2470
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2015-03-24 22:26:29 +0100

    Update and rename zadatak8.c to zad8.c

---
+++ LAB/Vezbe2/zad8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+main(){
+	
+	// xp - prethodno; xn - novo
+	float xp, xn, e;
+	
+	scanf("%f",&e);
+	xp = xn = 0;
+	do{
+		xp = xn;
+		xn = (3.0*powf(xp,4)+4.0*powf(xp,2)-4.0)/(4*powf(xp,3)+8*xp-9);
+		
+	} while (fabsf(xn - xp) >= e);
+	
+	printf("%f",xn);
+	
+
+}
+
+/* powf <=> pow. 
+ * powf funkcija uzima za parametar float i vraca rezultat float 
+ * pow funckija uzima za parametar double i vraca rezultat double
+ * 
+ * fabs <=> abs
+ * fabsf funkcija uzima za parametar float i vraca rezultat float
+ * abs funkcija uzima za parametar integer i vraca rezultat integer
+
+ */
+  
