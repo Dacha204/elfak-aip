5821a83854d9274c61d7a62d17943f4398159794
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2015-03-24 22:11:55 +0100

    Update zad6.c

--- LAB/Vezbe2/zad6.c
+++ LAB/Vezbe2/zad6.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
-int main(){
+ main(){
 	
 	int n,i,k;
-	double f1,f2;
+	double f1,f2; //moze int, long, float - double najveci opseg brojeva
 	
 	scanf("%d",&n);
 	
 	for (k = 2; k<=n; k++){
 		f1 = f2 = 1;
-		for (i=2; i <= k; f1 *= i++); //faktorijel od k
-		for (i=2; i <= k*k; f2 *= i++); //faktorijel od k*k 
+		
+		//faktorijel od k
+		for (i=2; i <= k; i++)
+			f1 = f1 * i;
+		
+		//faktorijel od k*k
+		for (i=2; i <= k*k; i++)
+			f2 = f2 * i; 
+			 
 		printf("f(%d)=%.0lf\tf(%d)=%.0lf\n",k,f1,k*k,f2);
 	}
-	return 0;
+
 }
