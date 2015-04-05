c1c20b2116166220e6d23501302f41083b84e3a4
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2015-04-06 00:33:02 +0200

    Create zad7.c

---
+++ LAB/Vezbe-3/zad7.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a[50],b[50],c[100];
+	int i,na,nb,nc;
+	int promena,pom;
+	
+	//unos niza A
+	printf("Niz A\nn=");
+	scanf("%d",&na);
+	printf("elementi:\n");
+	for (i=0; i<na; i++)
+		scanf("%d",&a[i]);
+	
+	//unos niza B
+	printf("Niz B\nn=");
+	scanf("%d",&nb);
+	printf("elementi: \n");
+	for (i=0; i<nb; i++)
+		scanf("%d",&b[i]);
+	
+	//formiranje niza C = A + B	
+	nc = na+nb;
+	for(i=0; i<na; i++)
+		c[i]=a[i];
+	
+	for (i=na; i<nc; i++)
+		c[i]=b[i-na];
+	
+	//sortiranje niza (bubble-sort)	
+	do{
+		promena = 0;
+		for (i=0; i<nc-1; i++)
+			if (c[i] > c[i+1]){
+				//zamena mesta
+				pom = c[i];
+				c[i] = c[i+1];
+				c[i+1] = pom;
+				promena = 1;
+			}		
+	}while (promena == 1);
+	
+	//stampanje niza C
+	printf("\n");
+	for (i=0; i<nc; i++)
+		printf("%d ", c[i]);
+	
+	return 0;
+}
