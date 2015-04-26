7527135d6b5769d290d95826b43e12766b71faea
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2015-04-26 21:47:28 +0200

    Update zad6-alt.c

--- LAB/Vezbe 2/zad6-alt.c
+++ LAB/Vezbe 2/zad6-alt.c
@@ -15,7 +15,6 @@ int main(){
 	return 0;
 }
 
-
 //pamti se vrednost od kog broja je izracunat u f2, zatim se racuna novi faktorijel u f2 od tog broja do sledeceg trazenog
 //npr. u f2 je faktorijel od 5. potrebno je za fakt 25. umesto ponovno racunanje fakt od 5 (pa do 25) nastavlja
 //se direktno racunanje od 5 do 25
